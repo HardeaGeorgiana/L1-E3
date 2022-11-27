d325ee67451597c063a2c0b52134b7b5f6ab12c6
Tenie Alexandru <alexandru_ale26@yahoo.com>
2022-11-27 13:34:20 +0200

    Add project files.

---
+++ L1 E3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace L1_E3
+{
+    /*E3. Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la 
+          tastaura*/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numar;
+
+            Console.WriteLine("Introduceti un numar");
+            numar = int.Parse(Console.ReadLine());
+            int ultimulNumar = numar % (10);
+
+            Console.WriteLine("Ultima cifra a numarului introdus este:" + ultimulNumar);
+        }
+    }
+}
