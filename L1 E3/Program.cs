2dc4691e45ea017d80b904777eb10337c22f1c0c
Hardea Georgiana <georgiana_hardea@yahoo.com>
2022-12-20 11:30:20 +0200

    final 2.0

--- L1 E3/Program.cs
+++ L1 E3/Program.cs
@@ -4,6 +4,7 @@ namespace L1_E3
 {
     /*E3. Scrieti un program care va afisa ultima cifra a unui numar intreg citit de la 
           tastaura*/
+
     class Program
     {
         static void Main(string[] args)
@@ -12,7 +13,7 @@ namespace L1_E3
 
             Console.WriteLine("Introduceti un numar");
             numar = int.Parse(Console.ReadLine());
-            int ultimulNumar = numar % (10);
+            int ultimulNumar = numar % 10;
 
             Console.WriteLine("Ultima cifra a numarului introdus este:" + ultimulNumar);
         }
